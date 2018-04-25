54f0093b5c6bd6f2b094f630bea09b630a520ce5
Andreu Ramos <aramos@civitfun.com>
2018-04-25 10:03:25 +0200

    Hero service, with Observable & subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular - Andreu Tour of Heroes ';
-  description = 'This is the angular tutorial description';
 }
